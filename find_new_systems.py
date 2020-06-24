7bb54f62b19fdc3083574bc825d343f9fff22800
Hanno Rein <hanno@hanno-rein.de>
2020-06-23 20:54:23 -0400

    compare

--- find_new_systems.py
+++ find_new_systems.py
@@ -20,20 +20,39 @@ def cleanplanet(name):
     return name
 
 
-@app.route('/systems_open_exoplanet_catalogue/<name>')
-def soec(name,d="/systems_open_exoplanet_catalogue/"):
-    h = "<xmp>"
-    with open("."+d+name, 'r') as content_file:
-        h += "".join(content_file.readlines())
-    h += "</xmp>"
+@app.route('/compare/<name1>/<name2>/')
+def compare(name1,name2):
+    h = "<frameset cols=\"50%,50%\">"
+    h += "<frame src=\"/systems_exoplanetarchive/"+name1+"\"/>"
+    h += "<frame src=\"/systems_open_exoplanet_catalogue/"+name2+"\"/>"
+    h += "</frameset>"
     return h
 
-@app.route('/systems_exoplanetarchive/<name>')
-def sea(name):
-    return soec(name,"/systems_exoplanetarchive/")
+
+@app.route('/<directory>/<name>')
+def showfile(directory,name):
+    h = """
+    <html>
+    <head>
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/styles/default.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/10.1.1/highlight.min.js"></script>
+    <script>hljs.configure({tabReplace: '    '});hljs.initHighlightingOnLoad();</script>
+    </head>
+    <body>
+    """
+    h += "<pre><code class='xml'>"
+    with open(directory+"/"+name, 'r') as content_file:
+        h += html.escape("".join(content_file.readlines()))
+    h += "</pre></code>"
+    h += """
+    </body>
+    </html>
+    """
+    return h
+
 
 @app.route('/')
-def hello():
+def index():
     planets_oec = {}
     for f in glob.iglob('systems_open_exoplanet_catalogue/*.xml'):
         system = ET.parse(f)
@@ -75,7 +94,6 @@ def hello():
     h += "<th>last-update</th>"
     h += "<th>discovery year</th>"
     h += "<th>planets found</th>"
-    h += "<th>planet already in oec?</th>"
     h += "<th>oec file</th>"
     h += "<th>last oec update</th>"
     h += "<th>actions</th>"
@@ -86,23 +104,36 @@ def hello():
         h += "<td><a href='/" + k + "'>"+ os.path.basename(k) + "</a></td>"
         h += "<td>" + systems_ea[k][0] + "</td>"
         h += "<td>" + str(systems_ea[k][1]) + "</td>"
-        h += "<td>" + str(", ".join(systems_ea[k][2])) + "</td>"
-        c = 0
-        cc = 0
         oecd = ["", ""]
+        pl = []
         for p in systems_ea[k][2]:
+            c = 0
             if p in planets_oec.keys():
-                c += 1
+                c = 1
                 oecd = planets_oec[p]
             else:
                 if p in planets_oec_clean.keys():
-                    cc += 1
+                    c = 2
                     oecd = planets_oec_clean[p]
+            if c==0:
+                pl.append(p)
+            elif c==1:
+                pl.append("<span style=\"background-color: #FF0000\">"+p+"</span>")
+            elif c==2:
+                pl.append("<span style=\"background-color: #FFAA00\">"+p+"</span>")
 
-        h += "<td>" + "%d/%d/%d" %(c, cc, len(systems_ea[k][2])) + "</td>"
+        h += "<td>" + ", ".join(pl) + "</td>"
+
         h += "<td><a href='/" + oecd[0] + "'>"+ os.path.basename(oecd[0]) + "</a></td>"
         h += "<td>" + oecd[1] + "</td>"
-        h += "<td>  <a href=''>add</a>  <a href=''>ignore</a> </td>"
+        h += "<td>"
+        if len(oecd[0])==0:
+            h += "compare "
+        else:
+            h += "<a href='/compare/"+os.path.basename(k) +"/" +os.path.basename(oecd[0])+"'>compare</a> "
+        h += "<a href=''>add</a> "
+        h += "<a href=''>ignore</a> "
+        h += "</td>"
         h += "</tr>"
     h += "</table>"
     h += """
